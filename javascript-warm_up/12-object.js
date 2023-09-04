c58153c480570c192518f60309ca2712a33ca9bf
giddy77 <giddysipoi@gmail.com>
2023-09-04 20:24:01 +0300

    accessing features ofan object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const myObject = {
     type: 'object',
-    value: 89
+    value: 12
   };
   console.log(myObject);
   /*
   YOUR CODE HERE
   */
+ myObject.value = 89
+  console.log(myObject);
