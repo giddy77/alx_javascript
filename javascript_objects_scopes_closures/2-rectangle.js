04fb303e34815ec7e0d7d02c336c8d4b9d20ceba
giddy77 <giddysipoi@gmail.com>
2023-09-07 22:23:48 +0300

    working with conditions

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,6 @@ class Rectangle {
     /**this is the rectangle class */
     constructor(w, h) {
         if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            // If w or h is less than or equal to zero, or not an integer, create an empty object
-            this.width = undefined;
-            this.height = undefined;
-        } else {
             this.width = w;
             this.height = h;
         }
