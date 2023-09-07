6b23dc485eee7739fd41cc9cbcf8215d2defe1ef
giddy77 <giddysipoi@gmail.com>
2023-09-07 22:19:28 +0300

    working with conditions

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
     /**this is the rectangle class */
-        constructor(w,h) {
-            if (w === undefined || h === undefined || w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-                // If w or h is less than or equal to zero, or not an integer, create an empty object
-                this.width = 0;
-                this.height = 0;
-              } else {
-                this.width = w;
-                this.height = h;
-              }
+    constructor(w, h) {
+        if (w === undefined || h === undefined || w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If w or h is less than or equal to zero, or not an integer, create an empty object
+            this.width = undefined;
+            this.height = undefined;
+        } else {
+            this.width = w;
+            this.height = h;
         }
     }
-    
-    module.exports = Rectangle;
-    
+}
+
+module.exports = Rectangle;
