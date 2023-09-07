e595ecef8b217505f53f01c39a2c84ad0801a3ff
giddy77 <giddysipoi@gmail.com>
2023-09-07 22:00:01 +0300

    constructor takes arguments

---
+++ javascript_objects_scopes_closures/-rectangle.js
@@ -0,0 +1,11 @@
+/**defining an empty class rectangel*/
+class Rectangle {
+    /**this is the rectangle class */
+        constructor(w,h) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    
+    module.exports = Rectangle;
+    
