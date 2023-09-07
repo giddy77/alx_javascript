0a9566f5435e5039055af461a18959124adb50e5
giddy77 <giddysipoi@gmail.com>
2023-09-07 23:16:59 +0300

    adding more conditions

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -10,13 +10,13 @@ class Rectangle {
       
 
 
-      print(){
-        for (i=0; i<count(this.width); i++){
-            console.log("X")
-        }
-
-        for (i=0; i<count(this.height); i++){
-            console.log("X")
+      print() {
+        for (let i = 0; i < this.height; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                row += 'X';
+            }
+            console.log(row);
         }
       }
       
