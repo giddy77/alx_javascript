69f3fd592d23fb5f61c7653f6d7351859be63d09
giddy77 <giddysipoi@gmail.com>
2023-09-07 22:46:22 +0300

    updates

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+    /**this is the rectangle class */
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+          // If w or h is less than or equal to zero, not an integer, or negative, create an empty object
+          return this.Rectangle;
+        } else {
+          this.width = w;
+          this.height = h;
+        }
+      }
+
+      function print(width, height) {
+        console.log("Width:", width);
+        console.log("Height:", height);
+      }
+      
+}
+
+module.exports = Rectangle;
