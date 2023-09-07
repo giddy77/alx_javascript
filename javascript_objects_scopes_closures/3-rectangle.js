1d4a80288391bbb06fa2c0f4df7b75efa61fa789
giddy77 <giddysipoi@gmail.com>
2023-09-07 23:07:39 +0300

    corrections

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,18 +2,10 @@
 class Rectangle {
     /**this is the rectangle class */
     constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-          // If w or h is less than or equal to zero, not an integer, or negative, create an empty object
-          return this.Rectangle;
-        } else {
-          this.width = w;
-          this.height = h;
+        if (w>0, h>0){
+            this.width = w;
+            this.height = h;
         }
-      }
-
-     print(width, height) {
-        console.log("Width:", width);
-        console.log("Height:", height);
       }
       
 }
