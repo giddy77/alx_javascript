4ab61b621aea47487ad1892ef763d07f779747e6
giddy77 <giddysipoi@gmail.com>
2023-09-07 23:31:47 +0300

    extending a class

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,9 @@
 const Rectangle = require("./0-rectangle");
 
 class Square extends Rectangle{
-    constructor(size){
+
+    constructor(w,h,size){
         this.size = size;
-        super(w,h);
+        super();
     }
 }
