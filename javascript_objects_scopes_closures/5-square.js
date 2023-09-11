4b04763e7c415fb4af286cf62d4a1f0aab33c476
giddy77 <giddysipoi@gmail.com>
2023-09-11 21:43:29 +0300

    corrections

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,23 @@
-const Rectangle = require("./4-rectangle");
+const Rectangle = require("./4-rectangle");// Replace with the actual path to your Rectangle class file
 
 class Square extends Rectangle {
-    constructor(size) {
-      super(size, size); // Call the constructor of the parent class (Rectangle)
+  constructor(size) {
+    super(size, size); // Call the constructor of the parent class (Rectangle)
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
   }
 
-module.export = Square;
+  double() {
+    // Double the width and height of the square
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
