8a924d3dada164fdf4f5fc1b4e9d999412d28965
giddy77 <giddysipoi@gmail.com>
2023-09-12 00:47:47 +0300

    get the status code of an api response

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -5,5 +5,5 @@ let result = [];
 const request = require('request');
 const response = request.get(url, {encoding:"utf-8"})
 .on('response',function(response){
-    console.log(response.statusCode)
+    console.log('code: ',response.statusCode)
 });
