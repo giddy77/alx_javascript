f10eb42b252b6afe63ed5bf5a4cedf8a4ab78eb9
giddy77 <giddysipoi@gmail.com>
2023-09-11 22:05:14 +0300

    corrections

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+// Import the Square class from 5-square.js (assuming the correct path)
+const ParentSquare = require('./5-square');
+
+// Define the Square class that inherits from ParentSquare
+class Square extends ParentSquare {
+  constructor(size) {
+    super(size); // Call the constructor of the parent class (ParentSquare) with size
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X'; // Default to 'X' if c is undefined
+    }
+
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+// Export the Square class
+module.exports = Square;
