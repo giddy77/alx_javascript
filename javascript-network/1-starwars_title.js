2379d44576404a1564fffdd674558ff403947cc9
giddy77 <giddysipoi@gmail.com>
2023-09-12 01:02:39 +0300

    getting the title of the star wars api objects

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,11 +1,12 @@
-const url = 'https://swapi-api.alx-tools.com/api/films/:id'
+let  id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/{id}'
 
 const request = require('request')
 
 const response = request.get(url,(error, body, code)=>{
     const body_json = JSON.parse(response.body);
 
-    const body_title = body.forEach( function(title){
+    const body_title = body_json.forEach( function(title){
         console.log(body_title.title);
     })
 })
