a79ac6e8d21c6b5e5b9695c830b950864780a16b
giddy77 <giddysipoi@gmail.com>
2023-09-11 21:40:53 +0300

    corrections

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,24 +1,9 @@
-class Rectangle {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-    }
-  
-    getArea() {
-      return this.width * this.height;
-    }
-  
-    getPerimeter() {
-      return 2 * (this.width + this.height);
-    }
-  }
-  
-  class Square extends Rectangle {
+const Rectangle = require("./4-rectangle");
+
+class Square extends Rectangle {
     constructor(size) {
       super(size, size); // Call the constructor of the parent class (Rectangle)
     }
-  
-    toString() {
-      return `Square: width=${this.width}, height=${this.height}`;
-    }
   }
+
+module.export = Square;
