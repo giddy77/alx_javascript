6e399d70c11783d94c4641a4c9cf529c6b729fdc
giddy77 <giddysipoi@gmail.com>
2023-09-12 12:07:38 +0300

    getting title from the api

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,12 +1,15 @@
-let  id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/{id}'
 
-const request = require('request')
+const id = process.argv[2]; // Use process.argv[2] to get the ID from command line argument
+const url = `https://swapi-api.alx-tools.com/api/films/${id}` 
 
-const response = request.get(url,(error, body, code)=>{
-    const body_json = JSON.parse(response.results);
+const request = require('request');
+let responseData = ''; // Initialize an empty string to accumulate response data
 
-    const body_title = body_json.forEach( function(title){
-        console.log(body_title.title);
-    })
-})
+const response = request.get(url, { encoding: "utf-8" })
+  .on('data', function(chunk) {
+    responseData +=chunk; // Accumulate response data
+  })
+  .on('end', function() {
+    const movie = JSON.parse(responseData); // Parse the accumulated data as JSON
+    console.log(movie.title);
+  });
