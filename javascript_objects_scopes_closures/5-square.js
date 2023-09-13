dca83136d052d8f2830aada1ea64ba700f1f8a23
giddy77 <giddysipoi@gmail.com>
2023-09-13 16:26:10 +0300

    updates

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,11 @@
+#!/usr/bin/node
 const Rectangle = require("./4-rectangle");// Replace with the actual path to your Rectangle class file
 
 class Square extends Rectangle {
   constructor(w,h,size) {
-    super(size, size);
+    super(w,h, size);
     this.size = size; // Call the constructor of the parent class (Rectangle)
   }
 }
+
+module.exports = Square;
