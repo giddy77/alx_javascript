9e50ec3288a52484bf81f0675e468ae5d40b0aae
giddy77 <giddysipoi@gmail.com>
2023-09-16 13:50:19 +0300

    updates

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,4 +1,5 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const request = require('request');
 
 request.get(apiUrl, (error, response, body) => {
     if (error) {
@@ -6,18 +7,25 @@ request.get(apiUrl, (error, response, body) => {
     } else if (response && response.statusCode === 200) {
         const todos = JSON.parse(body);
 
-        // Create an object to store the task counts for each user
-        const userTaskCounts = {};
+        // Create an object to store the completed task counts for each user
+        const completedTasksByUser = {};
 
         // Iterate through the todos and count completed tasks for each user
         todos.forEach((todo) => {
-            if (todo.completed) {
-                if (userTaskCounts[todo.userId]) {
-                    userTaskCounts[todo.userId]++;
-                } 
-                    userTaskCounts[todo.userId] = 1;
-            
+            if (todo.completed = true) {
+                if (completedTasksByUser[todo.userId]) {
+                    completedTasksByUser[todo.userId]++;
+                } else {
+                    completedTasksByUser[todo.userId] = 1;
+                }
             }
         });
+
+        // Print the number of completed tasks by each user ID
+        for (const userId in completedTasksByUser) {
+            console.log(`'${userId}': ${completedTasksByUser[userId]},`);
+        }
+    } else {
+        console.error('HTTP request failed with status code:', response ? response.statusCode : 'unknown');
     }
-    });
+});
