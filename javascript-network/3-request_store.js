52f4f669177cda7db05afac34630f1157783a0e5
giddy77 <giddysipoi@gmail.com>
2023-09-16 12:11:07 +0300

    corrections

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const url = process.argv[2];
 let filePath = process.argv[3];
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    }
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
         if (writeError) {
             console.error(writeError);
