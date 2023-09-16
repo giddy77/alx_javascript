6e82f14b3301b075361c264563983ff43cb2acea
giddy77 <giddysipoi@gmail.com>
2023-09-16 12:12:34 +0300

    updates

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,11 +3,8 @@ const fs = require('fs');
 const url = process.argv[2];
 let filePath = process.argv[3];
 request.get(url, (error, response, body) => {
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-        if (writeError) {
-            console.error(writeError);
-        } else {
+    fs.writeFile(filePath, body, 'utf-8')
             console.log(filePath);
-        }
+        
     });
-});
+
