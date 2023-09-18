a926dada6a3593cffc967f955372809d63a99ef7
giddy77 <giddysipoi@gmail.com>
2023-09-18 12:26:49 +0300

    updates

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,29 +1,20 @@
 const request = require('request');
-
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-// Make a request to fetch the data from the API
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response && response.statusCode === 200) {
-    const todosData = JSON.parse(body);
-
-    // Initialize an object to store the count of completed tasks by user id
-    const completedTasksByUserId = {};
-    let userId = "";
-
-// Loop through the tasks and count completed tasks for each userId
-for (const task of todosData) {
-    const userId = task.userId;
-    if (task.completed === true) {
-        if (completedTasksByUserId[userId]) {
-            completedTasksByUserId[userId]++;
-        } else {
-            completedTasksByUserId[userId] = 1;
-        }
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
     }
-}
-console.log(`${userId} : ${completedTasksByUserId}`)
-}
-});
+    const tasks = JSON.parse(body);
+    const completed = tasks.filter(task => task.completed);
+    let counts = {};
+    completed.forEach(task => {
+        const id = task.userId;
+        if (!counts[id]) {
+            counts[id] = 1;
+        } else {
+            counts[id]++;
+        }
+    });
+        console.log(counts);
+
+})
