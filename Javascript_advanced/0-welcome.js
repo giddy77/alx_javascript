33301b3931eca52991b1d7606d02dc93766c4ac7
giddy77 <giddysipoi@gmail.com>
2023-09-18 15:32:37 +0300

    closure scope chain

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,8 +1,10 @@
 function welcome(firstName, lastName){
     let fullName = firstName + ' ' + lastName;
     function displayFullName(){
-        alert('welcome ',fullName+'!');
+        alert('welcome '+fullName +'!');
     }
 
     displayFullName();
 }
+
+welcome('Gideon','Sipoi');
